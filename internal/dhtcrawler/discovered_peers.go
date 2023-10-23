ae3a0edc40b8b2f637f475a5c9820ea6e842881c
Mike Gibson <mike@mgdigital.co.uk>
2023-10-23 09:42:54 +0100

    Remove commented code

--- internal/dhtcrawler/discovered_peers.go
+++ internal/dhtcrawler/discovered_peers.go
@@ -26,22 +26,6 @@ func (c *crawler) awaitDiscoveredPeers(ctx context.Context) {
 					}
 				}
 			}
-			//// before pinging a discovered peer we'll check if we already know about it;
-			//// we may or may not know the peer ID, and that will determine how we'll carry out this check
-			//if p.ID().IsZero() {
-			//	// we don't know the peer ID, so we'll do a reverse lookup on the address
-			//	if c.kTable.HasAddr(p.Addr().Addr()) {
-			//		break
-			//	}
-			//} else {
-			//	// we know the peer ID, so we can check if it's already in the routing table
-			//	if c.kTable.HasPeer(p.ID()) {
-			//		break
-			//	}
-			//}
-			//if err := c.peersForSampleInfoHashes.TryIn(p); err != nil {
-			//	_ = c.peersForFindNode.In(ctx, p)
-			//}
 		}
 	}
 }
