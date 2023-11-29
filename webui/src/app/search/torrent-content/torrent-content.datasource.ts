f55d43030373023281e437c970d40005efc0875a
mgdigital <mgdigital@users.noreply.github.com>
2023-11-29 06:42:11 +0000

    Split webUI query (#82)

--- webui/src/app/search/torrent-content/torrent-content.datasource.ts
+++ webui/src/app/search/torrent-content/torrent-content.datasource.ts
@@ -5,15 +5,17 @@ import { GraphQLService } from "../../graphql/graphql.service";
 import { AppErrorsService } from "../../app-errors.service";
 import { GenreAgg, VideoResolutionAgg, VideoSourceAgg } from "./facet";
 
+const emptyResult: generated.TorrentContentSearchResult = {
+  items: [],
+  totalCount: 0,
+  aggregations: {},
+};
+
 export class TorrentContentDataSource
   implements DataSource<generated.TorrentContent>
 {
   private resultSubject =
-    new BehaviorSubject<generated.TorrentContentSearchResult>({
-      items: [],
-      totalCount: 0,
-      aggregations: {},
-    });
+    new BehaviorSubject<generated.TorrentContentSearchResult>(emptyResult);
   private inputSubject =
     new BehaviorSubject<generated.TorrentContentSearchQueryVariables>({});
   private loadingSubject = new BehaviorSubject(false);
@@ -61,23 +63,34 @@ export class TorrentContentDataSource
     const requestTime = Date.now();
     this.loadingSubject.next(true);
     this.inputSubject.next(input);
-    this.graphQLService
-      .torrentContentSearch(input)
-      .pipe(
-        catchError((err: Error) => {
-          this.errorsService.addError(`Error loading results: ${err.message}`);
-          this.loadingSubject.next(false);
-          return EMPTY;
-        }),
-      )
-      .subscribe((result) => {
-        const lastRequestTime = this.lastRequestTimeSubject.getValue();
-        if (requestTime > lastRequestTime) {
-          this.lastRequestTimeSubject.next(requestTime);
-          this.resultSubject.next(result);
-          this.loadingSubject.next(false);
-        }
-      });
+    let result = this.resultSubject.getValue();
+    let i = 0;
+    const split = splitQuery(input);
+    for (const { query, merge } of split) {
+      this.graphQLService
+        .torrentContentSearch(query)
+        .pipe(
+          catchError((err: Error) => {
+            this.errorsService.addError(
+              `Error loading results: ${err.message}`,
+            );
+            this.loadingSubject.next(false);
+            return EMPTY;
+          }),
+        )
+        .subscribe((thisResult) => {
+          const lastRequestTime = this.lastRequestTimeSubject.getValue();
+          if (requestTime >= lastRequestTime) {
+            result = merge(result, thisResult);
+            i++;
+            this.lastRequestTimeSubject.next(requestTime);
+            this.resultSubject.next(result);
+            if (i === split.length) {
+              this.loadingSubject.next(false);
+            }
+          }
+        });
+    }
   }
 
   refreshResult(): void {
@@ -92,3 +105,50 @@ export class TorrentContentDataSource
     return this.loadResult(uncachedInput);
   }
 }
+
+type SplitQueryPart = {
+  query: generated.TorrentContentSearchQueryVariables;
+  merge: (
+    prev: generated.TorrentContentSearchResult,
+    next: generated.TorrentContentSearchResult,
+  ) => generated.TorrentContentSearchResult;
+};
+
+/**
+ * Split a query into two parts: one that fetches the items and one that fetches aggregations
+ */
+const splitQuery = (
+  input: generated.TorrentContentSearchQueryVariables,
+): SplitQueryPart[] => [
+  {
+    query: {
+      query: {
+        ...input.query,
+        totalCount: false,
+      },
+      facets: Object.fromEntries(
+        Object.entries(input.facets ?? {}).map(([key, value]) => [
+          key,
+          { ...value, aggregate: false },
+        ]),
+      ),
+    },
+    merge: (prev, next) => ({
+      ...prev,
+      items: next.items,
+    }),
+  },
+  {
+    query: {
+      query: {
+        ...input.query,
+        limit: 0,
+      },
+      facets: input.facets,
+    },
+    merge: (prev, next) => ({
+      ...next,
+      items: prev.items,
+    }),
+  },
+];
