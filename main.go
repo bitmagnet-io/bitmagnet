a8c57476e4eeb5fad1e6a6f636b59797024a74b4
Mike Gibson <mike@mgdigital.co.uk>
2024-01-06 12:46:34 +0000

    Remove commented code

--- main.go
+++ main.go
@@ -6,11 +6,5 @@ import (
 )
 
 func main() {
-	//fmt.Printf("%#v\n", unidecode.Unidecode("我"))
-	//println(unicode.IsLetter('1'))
 	app.New().Run()
-	//fmt.Printf("%#v\n", tsvector.Tokenize("hello 555 world2010昨 日 お 店に行ってサンドイッチを購入しました hôtel Київ ℂ 안녕하세요, 샌드위치 하나 먹어도 될까요? مرحباً، هل لي أن أتناول شطيرة؟ 你好，我可以吃三明治吗？ สวัสดีครับ ฉันสามารถกินแซนวิชได้ไหมครับ?"))
-	//fmt.Printf("%#v\n", tsvector.Tokenize("مرحباً، هل لي أن أتناول شطيرة؟"))
-	//fmt.Printf("%#v\n", tsvector.Tokenize("hello world"))
-	//println(tsvector.ParseQuery("Hello world foo 你好，我可以吃!!!|(我可以吃三明治吗？店に行ってサンドイッチを購入しました) | (fat !cat) | mat"))
 }
