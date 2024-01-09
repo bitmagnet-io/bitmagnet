e66069845c4c38c4ed5526d3f53775d71592b4b4
Mike Gibson <mike@mgdigital.co.uk>
2024-01-09 12:06:57 +0000

    Disable gorm cache easer

--- internal/database/cache/config.go
+++ internal/database/cache/config.go
@@ -12,7 +12,10 @@ type Config struct {
 func NewDefaultConfig() Config {
 	return Config{
 		CacheEnabled: true,
-		EaserEnabled: true,
+		// The easer has been disabled as it seems to cause an insidious bug whereby zero results are sometimes incorrectly returned;
+		// if I can get time to understand the problem better I may open an issue in https://github.com/go-gorm/caches, though they
+		// don't seem very responsive to issues, hence why bitmagnet uses a forked version of this library...
+		EaserEnabled: false,
 		Ttl:          time.Minute * 60,
 		MaxKeys:      1000,
 	}
