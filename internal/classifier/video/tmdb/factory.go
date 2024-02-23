10532096129e412366683eb02373a9ad80dcdddb
mgdigital <mgdigital@users.noreply.github.com>
2024-02-23 14:38:32 +0000

    Replace TMDB client with custom implementation (#168)
    - Replace TMDB client with custom implementation (the previous client caused problems due to not using the Go context convention and imposing its own timeout)
    - Check TMDB API key validity on startup
    - Fall back to default TMDB API key when configured key is invalid
    - Don't continue calling TMDB after receiving a 401
    
    Fixes https://github.com/bitmagnet-io/bitmagnet/issues/157

--- internal/classifier/video/tmdb/factory.go
+++ internal/classifier/video/tmdb/factory.go
@@ -1,22 +1,18 @@
 package tmdb
 
 import (
-	"github.com/bitmagnet-io/bitmagnet/internal/boilerplate/httpclient/httplogger"
-	"github.com/bitmagnet-io/bitmagnet/internal/boilerplate/httpclient/httpratelimiter"
 	"github.com/bitmagnet-io/bitmagnet/internal/boilerplate/lazy"
 	"github.com/bitmagnet-io/bitmagnet/internal/database/search"
-	tmdb "github.com/cyruzin/golang-tmdb"
+	"github.com/bitmagnet-io/bitmagnet/internal/tmdb"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
-	"net/http"
-	"time"
 )
 
 type Params struct {
 	fx.In
-	Config Config
-	Logger *zap.SugaredLogger
-	Search lazy.Lazy[search.Search]
+	Search     lazy.Lazy[search.Search]
+	TmdbClient lazy.Lazy[tmdb.Client]
+	Logger     *zap.SugaredLogger
 }
 
 type Result struct {
@@ -31,30 +27,9 @@ func New(p Params) Result {
 			if err != nil {
 				return nil, err
 			}
-			logger := p.Logger.Named("tmdb_client")
-			rateLimit := p.Config.RateLimit
-			rateLimitBurst := p.Config.RateLimitBurst
-			if p.Config.ApiKey == defaultTmdbApiKey {
-				rateLimit = time.Second
-				rateLimitBurst = 1
-				logger.Warnln("you are using the default TMDB api key; TMDB requests will be limited to 1 per second; to remove this warning please configure a personal TMDB api key")
-			}
-			httpClient := http.Client{
-				// need to set a non-zero value as the underlying client unfortunately sets 10 seconds as the default if none is provided;
-				// this does not work well with the rate limiter; a 30 second timeout fixes this assuming a concurrency of 10 on the queue
-				// (and a maximum of 2 TMDB requests per classification)
-				Timeout: time.Second * 30,
-				Transport: httpratelimiter.NewDecorator(
-					rateLimit,
-					rateLimitBurst,
-				)(httplogger.NewDecorator(
-					logger,
-				)(http.DefaultTransport)),
-			}
-			c, initErr := tmdb.Init(p.Config.ApiKey)
-			c.SetClientConfig(httpClient)
-			if initErr != nil {
-				return nil, initErr
+			c, err := p.TmdbClient.Get()
+			if err != nil {
+				return nil, err
 			}
 			return &client{
 				c: c,
