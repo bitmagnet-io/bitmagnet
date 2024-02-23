332cb51e0775d45fe49feae42ed7d24d65d838fa
Mike Gibson <mike@mgdigital.co.uk>
2024-02-23 14:06:41 +0000

    Replace TMDB client with custom implementation

---
+++ internal/tmdb/config.go
@@ -0,0 +1,23 @@
+package tmdb
+
+import "time"
+
+type Config struct {
+	ApiKey         string
+	RateLimit      time.Duration
+	RateLimitBurst int
+}
+
+func NewDefaultConfig() Config {
+	return Config{
+		ApiKey:         defaultTmdbApiKey,
+		RateLimit:      defaultRateLimit,
+		RateLimitBurst: defaultRateLimitBurst,
+	}
+}
+
+const (
+	defaultTmdbApiKey     = "9c6689fa83ae6814fbfb200d70bba3a8"
+	defaultRateLimit      = time.Second / 20
+	defaultRateLimitBurst = 5
+)
