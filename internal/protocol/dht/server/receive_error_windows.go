ce10f4cb260fa76c26f27f5ccdca048d50dcbe22
Mike Gibson <mike@mgdigital.co.uk>
2024-03-08 14:55:13 +0000

    Update ignoreReadFromError

--- internal/protocol/dht/server/receive_error_windows.go
+++ internal/protocol/dht/server/receive_error_windows.go
@@ -2,13 +2,23 @@ package server
 
 import (
 	"errors"
+	"golang.org/x/sys/windows"
 	"syscall"
 )
 
 func ignoreReadFromError(err error) bool {
 	var errno syscall.Errno
-	if !errors.As(err, &errno) {
-		return false
+	if errors.As(err, &errno) {
+		switch errno {
+		case
+			windows.WSAENETRESET,
+			windows.WSAECONNRESET,
+			windows.WSAECONNABORTED,
+			windows.WSAECONNREFUSED,
+			windows.WSAENETUNREACH,
+			windows.WSAETIMEDOUT:
+			return true
+		}
 	}
-	return errno == syscall.WSAECONNRESET
+	return false
 }
