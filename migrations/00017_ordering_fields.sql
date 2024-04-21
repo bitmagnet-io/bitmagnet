52559262746915d68c54871c4b2782296cfd4e6e
mgdigital <mgdigital@users.noreply.github.com>
2024-04-21 22:44:14 +0100

    Fix slow migration (#242)
    * Remove field update

--- migrations/00017_ordering_fields.sql
+++ migrations/00017_ordering_fields.sql
@@ -1,12 +1,8 @@
 -- +goose Up
 -- +goose StatementBegin
 
--- Fix an egregious error that has been there from day 1:
-update torrents_torrent_sources set seeders = leechers, leechers = seeders where source = 'dht';
-
 -- Drop some fairly useless columns that take up a lot of space:
-alter table torrents_torrent_sources drop column bfsd;
-alter table torrents_torrent_sources drop column bfpe;
+alter table torrents_torrent_sources drop column bfsd, drop column bfpe;
 
 alter table torrent_contents add column seeders integer;
 alter table torrent_contents add column leechers integer;
@@ -26,8 +22,6 @@ create index on torrent_contents (published_at);
 alter table torrent_contents drop column seeders;
 alter table torrent_contents drop column leechers;
 alter table torrent_contents drop column published_at;
-
-update torrents_torrent_sources set seeders = leechers, leechers = seeders where source = 'dht';
 
 alter table torrents_torrent_sources add column bfsd bytea;
 alter table torrents_torrent_sources add column bfpe bytea;
