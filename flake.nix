46a1b95011cc7eca94b460450e1c02a53c6d38ec
Mike Gibson <mike@mgdigital.co.uk>
2024-04-30 14:00:11 +0100

    WIP

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            bundler
+            go
+            go-task
+            jekyll
+            nodejs_20
+            protobuf
+            protoc-gen-go
+          ];
+        };
+      });
+}
