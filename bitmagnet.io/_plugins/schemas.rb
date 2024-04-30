b7113256cae5e67c4664a1052d177b489a1d9092
mgdigital <mgdigital@users.noreply.github.com>
2024-04-30 18:46:24 +0100

    Fix docsite plugin (#252)

--- bitmagnet.io/_plugins/schemas.rb
+++ bitmagnet.io/_plugins/schemas.rb
@@ -1,6 +1,9 @@
+require 'fileutils'
+
 module Schemas
   class Generator < Jekyll::Generator
     def generate(site)
+      FileUtils.mkdir_p File.join(site.dest, 'schemas')
       Dir.glob(File.join(site.source, 'schemas', '*.json')).each do |json_file|
         File.open(File.join(site.dest, 'schemas', File.basename(json_file)), 'w') do |file|
           file.write(File.read(json_file))
