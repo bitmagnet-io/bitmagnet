42bee4bbb59c5d90d53c3c18df8fb761483709c1
Mike Gibson <mike@mgdigital.co.uk>
2024-04-30 17:09:36 +0100

    nix fmt

--- flake.nix
+++ flake.nix
@@ -3,26 +3,29 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells = {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              bundler
-              go_1_22
-              go-task
-              golangci-lint
-              jekyll
-              nodejs_20
-              nodePackages.prettier
-              protobuf
-              protoc-gen-go
-            ];
-          };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      formatter = pkgs.alejandra;
+      devShells = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            bundler
+            go_1_22
+            go-task
+            golangci-lint
+            jekyll
+            nodejs_20
+            nodePackages.prettier
+            protobuf
+            protoc-gen-go
+          ];
         };
-      });
+      };
+    });
 }
