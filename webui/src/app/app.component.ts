0e8d8f8d34544d31969fd1214cbbf52070679aa4
Mike Gibson <mike@mgdigital.co.uk>
2024-05-19 17:00:32 +0100

    Dark mode WIP

--- webui/src/app/app.component.ts
+++ webui/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "bitmagnet";
+  theme: Theme = "light";
+
+  constructor() {
+    if (window.matchMedia("(prefers-color-scheme)").media !== "not all") {
+      const darkModeMediaQuery = window.matchMedia(
+        "(prefers-color-scheme: dark)",
+      );
+      this.theme = darkModeMediaQuery.matches ? "dark" : "light";
+      darkModeMediaQuery.addEventListener("change", (e) => {
+        this.theme = e.matches ? "dark" : "light";
+      });
+    }
+  }
 }
+
+type Theme = "light" | "dark";
