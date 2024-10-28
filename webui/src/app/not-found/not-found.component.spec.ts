f3a4913eb9ba996074ed6d4d0e4724a8361b8f5e
Mike Gibson <mike@mgdigital.co.uk>
2024-10-28 11:31:06 +0000

    Linting

--- webui/src/app/not-found/not-found.component.spec.ts
+++ webui/src/app/not-found/not-found.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
+import { appConfig } from "../app.config";
 import { NotFoundComponent } from "./not-found.component";
-import {appConfig} from "../app.config";
 
 describe("NotFoundComponent", () => {
   let component: NotFoundComponent;
