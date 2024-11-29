a397c471ccec0beb524331d0d74bf0cff641187f
mgdigital <mgdigital@users.noreply.github.com>
2024-11-29 18:16:59 +0000

    Add page titles (#355)
    * Add page titles
    
    * Rebuild webui

--- webui/src/app/dashboard/queue/queue-admin.component.ts
+++ webui/src/app/dashboard/queue/queue-admin.component.ts
@@ -1,13 +1,14 @@
 import { Component, inject } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { AppModule } from "../../app.module";
+import { DocumentTitleComponent } from "../../layout/document-title.component";
 import { QueueEnqueueReprocessTorrentsBatchDialogComponent } from "./queue-enqueue-reprocess-torrents-batch-dialog.component";
 import { QueuePurgeJobsDialogComponent } from "./queue-purge-jobs-dialog.component";
 
 @Component({
   selector: "app-queue-admin",
   standalone: true,
-  imports: [AppModule],
+  imports: [AppModule, DocumentTitleComponent],
   templateUrl: "./queue-admin.component.html",
   styleUrl: "./queue-admin.component.scss",
 })
