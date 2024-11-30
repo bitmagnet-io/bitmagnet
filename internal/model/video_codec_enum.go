7f67fd7154ac2a7e3633c179bb25ba84d7252f0c
Mike Gibson <mike@mgdigital.co.uk>
2024-11-30 11:50:43 +0000

    Fix GraphQL unmarshal of nullable enum

--- internal/model/video_codec_enum.go
+++ internal/model/video_codec_enum.go
@@ -221,5 +221,8 @@ func (n *NullVideoCodec) UnmarshalGQL(v any) error {
 	if !ok {
 		return errors.New("value is not a string")
 	}
-	return n.UnmarshalJSON([]byte(str))
+	if str == "null" {
+		return nil
+	}
+	return n.UnmarshalJSON([]byte("\"" + str + "\""))
 }
