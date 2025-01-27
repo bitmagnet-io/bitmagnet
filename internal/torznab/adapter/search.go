721efcf37389878e7b14a37a0c76ec6f116875f1
rraymondgh <rob.j.raymond@gmail.com>
2025-01-27 17:10:57 +0000

    torznab - permalinks and tmdb id in results
    Revert "torznab - permalinks and tmdb id in results"
    
    This reverts commit 08bc7f797ccdee0a6684b888a7b92af0a6264254.
    
    torznab - tmdb id in results

--- internal/torznab/adapter/search.go
+++ internal/torznab/adapter/search.go
@@ -221,6 +221,12 @@ func (a adapter) transformSearchResult(req torznab.SearchRequest, res search.Tor
 				AttrValue: item.PublishedAt.Format(torznab.RssDateDefaultFormat),
 			},
 		}
+		if item.ContentID.Valid {
+			attrs = append(attrs, torznab.SearchResultItemTorznabAttr{
+				AttrName:  torznab.AttrTmdb,
+				AttrValue: item.ContentID.String,
+			})
+		}
 		seeders := item.Torrent.Seeders()
 		leechers := item.Torrent.Leechers()
 		if seeders.Valid {
