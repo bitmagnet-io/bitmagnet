284a10d249bdccfa1e107a7ddf7080c672ca3432
mgdigital <mgdigital@users.noreply.github.com>
2025-02-09 11:20:34 +0000

    Fix search params (#380)
    There were some issues with handling of URL parameters in the search interface, which was causing anomalies with filtering and editing tags.

--- webui/src/app/torrents/torrents-search.component.ts
+++ webui/src/app/torrents/torrents-search.component.ts
@@ -40,13 +40,18 @@ import {
 import {
   ContentTypeSelection,
   defaultOrderBy,
-  defaultQueryOrderBy,
   FacetInfo,
   facets,
   inactiveFacet,
+  isDefaultOrdering,
   orderByOptions,
+  OrderBySelection,
   TorrentSearchControls,
+  TorrentSelection,
   TorrentsSearchController,
+  TorrentTab,
+  torrentTabNames,
+  TorrentTabSelection,
 } from "./torrents-search.controller";
 
 @Component({
@@ -77,7 +82,7 @@ export class TorrentsSearchComponent implements OnInit, OnDestroy {
 
   controller: TorrentsSearchController;
 
-  controls: TorrentSearchControls;
+  controls = initControls;
 
   contentTypes = contentTypeList;
   orderByOptions = orderByOptions;
@@ -92,7 +97,8 @@ export class TorrentsSearchComponent implements OnInit, OnDestroy {
 
   result = emptyResult;
 
-  selection = new SelectionModel<string>(true, []);
+  multiSelection = new SelectionModel<string>(true, []);
+
   private selectedItemsSubject = new BehaviorSubject<
     generated.TorrentContent[]
   >([]);
@@ -101,10 +107,6 @@ export class TorrentsSearchComponent implements OnInit, OnDestroy {
   private subscriptions = Array<Subscription>();
 
   constructor() {
-    this.controls = {
-      ...initControls,
-      language: this.transloco.getActiveLang(),
-    };
     this.controller = new TorrentsSearchController(this.controls);
     this.dataSource = new TorrentsSearchDatasource(
       this.apollo,
@@ -144,8 +146,8 @@ export class TorrentsSearchComponent implements OnInit, OnDestroy {
         const infoHashes = new Set(
           result.items.map(({ infoHash }) => infoHash),
         );
-        this.selection.deselect(
-          ...this.selection.selected.filter(
+        this.multiSelection.deselect(
+          ...this.multiSelection.selected.filter(
             (infoHash) => !infoHashes.has(infoHash),
           ),
         );
@@ -156,63 +158,17 @@ export class TorrentsSearchComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.subscriptions.push(
       this.route.queryParams.subscribe((params) => {
-        const queryString = stringParam(params, "query");
-        this.queryString.setValue(queryString ?? null);
-        this.controller.update((ctrl) => {
-          const activeFacets = stringListParam(params, "facets");
-          let orderBy = ctrl.orderBy;
-          if (queryString) {
-            if (queryString !== ctrl.queryString) {
-              orderBy = defaultQueryOrderBy;
-            }
-          } else if (orderBy.field === "relevance") {
-            orderBy = defaultOrderBy;
-          }
-          return {
-            ...ctrl,
-            queryString,
-            orderBy,
-            contentType: contentTypeParam(params, "content_type"),
-            limit: intParam(params, "limit") ?? ctrl.limit,
-            page: intParam(params, "page") ?? ctrl.page,
-            facets: facets.reduce<TorrentSearchControls["facets"]>(
-              (acc, facet) => {
-                const active = activeFacets?.includes(facet.key) ?? false;
-                const filter = stringListParam(params, facet.key);
-                return facet.patchInput(acc, {
-                  active,
-                  filter,
-                });
-              },
-              ctrl.facets,
-            ),
-          };
-        });
+        this.queryString.setValue(stringParam(params, "query") ?? null);
+        this.controller.update(() => paramsToControls(params));
       }),
       this.controller.controls$.subscribe((ctrl) => {
-        let page: number | undefined = ctrl.page;
-        let limit: number | undefined = ctrl.limit;
-        if (page === 1) {
-          page = undefined;
-        }
-        if (limit === defaultLimit) {
-          limit = undefined;
-        }
         void this.router.navigate([], {
           relativeTo: this.route,
-          queryParams: {
-            query: ctrl.queryString
-              ? encodeURIComponent(ctrl.queryString)
-              : undefined,
-            page,
-            limit,
-            content_type: ctrl.contentType,
-            ...flattenFacets(ctrl.facets),
-          },
-          queryParamsHandling: "merge",
+          queryParams: controlsToParams(ctrl),
+          queryParamsHandling: "replace",
         });
       }),
-      this.selection.changed.subscribe((selection) => {
+      this.multiSelection.changed.subscribe((selection) => {
         const infoHashes = new Set(selection.source.selected);
         this.selectedItemsSubject.next(
           this.result.items.filter((i) => infoHashes.has(i.infoHash)),
@@ -230,7 +186,6 @@ export class TorrentsSearchComponent implements OnInit, OnDestroy {
 const defaultLimit = 20;
 
 const initControls: TorrentSearchControls = {
-  language: "en",
   page: 1,
   limit: defaultLimit,
   contentType: null,
@@ -246,12 +201,97 @@ const initControls: TorrentSearchControls = {
   },
 };
 
-const contentTypeParam = (
-  params: Params,
-  key: string,
-): ContentTypeSelection => {
-  const str = stringParam(params, key);
+const paramsToControls = (params: Params): TorrentSearchControls => {
+  const queryString = stringParam(params, "query");
+  const activeFacets = stringListParam(params, "facets");
+  let selectedTorrent: TorrentSelection | undefined;
+  const selectedTorrentParam = stringParam(params, "torrent");
+  if (selectedTorrentParam) {
+    let torrentTabSelection: TorrentTabSelection;
+    const strTab = stringParam(params, "tab");
+    if (torrentTabNames.includes(strTab as TorrentTab)) {
+      torrentTabSelection = strTab as TorrentTab;
+    }
+    selectedTorrent = {
+      infoHash: selectedTorrentParam,
+      tab: torrentTabSelection,
+    };
+  }
+  return {
+    queryString,
+    orderBy: orderByParam(params, !!queryString),
+    contentType: contentTypeParam(params),
+    limit: intParam(params, "limit") ?? defaultLimit,
+    page: intParam(params, "page") ?? 1,
+    selectedTorrent,
+    facets: facets.reduce<TorrentSearchControls["facets"]>((acc, facet) => {
+      const active = activeFacets?.includes(facet.key) ?? false;
+      const filter = stringListParam(params, facet.key);
+      return facet.patchInput(acc, {
+        active,
+        filter,
+      });
+    }, initControls.facets),
+  };
+};
+
+const controlsToParams = (ctrl: TorrentSearchControls): Params => {
+  let page: number | undefined = ctrl.page;
+  let limit: number | undefined = ctrl.limit;
+  if (page === 1) {
+    page = undefined;
+  }
+  if (limit === defaultLimit) {
+    limit = undefined;
+  }
+  const orderBy = isDefaultOrdering(ctrl) ? undefined : ctrl.orderBy;
+  let desc: string | undefined;
+  if (orderBy) {
+    desc = orderBy.descending ? "1" : "0";
+  }
+  return {
+    query: ctrl.queryString ? encodeURIComponent(ctrl.queryString) : undefined,
+    page,
+    limit,
+    content_type: ctrl.contentType,
+    order: orderBy?.field,
+    desc,
+    ...(ctrl.selectedTorrent
+      ? {
+          torrent: ctrl.selectedTorrent.infoHash,
+          tab: ctrl.selectedTorrent.tab ?? undefined,
+        }
+      : {}),
+    ...flattenFacets(ctrl.facets),
+  };
+};
+
+const contentTypeParam = (params: Params): ContentTypeSelection => {
+  const str = stringParam(params, "content_type");
   return str && str in contentTypeMap ? (str as ContentTypeSelection) : null;
+};
+
+const orderByParam = (params: Params, hasQuery: boolean): OrderBySelection => {
+  let desc: boolean | null = null;
+  const strDesc = stringParam(params, "desc");
+  if (strDesc === "1") {
+    desc = true;
+  } else if (strDesc === "0") {
+    desc = false;
+  }
+  const field = stringParam(params, "order");
+  for (const opt of orderByOptions) {
+    if (opt.field === field) {
+      return {
+        field,
+        descending: desc ?? opt.descending,
+      };
+    }
+  }
+  return {
+    field: hasQuery ? "relevance" : "published_at",
+    descending: desc ?? true,
+  };
 };
 
 const flattenFacets = (
