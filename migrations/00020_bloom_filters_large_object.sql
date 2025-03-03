e8ec731d6a489935f00f22986aaeeb23665c0364
Mike Gibson <mike@mgdigital.co.uk>
2025-03-03 08:06:46 +0000

    Fix migration

--- migrations/00020_bloom_filters_large_object.sql
+++ migrations/00020_bloom_filters_large_object.sql
@@ -11,7 +11,7 @@ alter table bloom_filters drop column bytes;
 -- +goose StatementBegin
 
 alter table bloom_filters add column bytes bytea;
-update bloom_filters set bytes = decode('') where true;
+update bloom_filters set bytes = ''::bytea where true;
 alter table bloom_filters alter column bytes set not null;
 alter table bloom_filters drop column oid;
 
