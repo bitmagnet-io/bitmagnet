23ae91529a350c04cfde7757c6bd3de5953e29ba
rraymondgh <rob.j.raymond@gmail.com>
2025-11-25 10:50:59 +0000

    ubuntu 24.04+ apparmor chromium,  nix 24.11 golangci-lint

--- webui/karma.conf.js
+++ webui/karma.conf.js
@@ -30,7 +30,13 @@ module.exports = function (config) {
       reporters: [{ type: "html" }, { type: "text-summary" }],
     },
     reporters: ["progress", "kjhtml"],
-    browsers: ["ChromeHeadless"],
+    browsers: ["ChromeHeadlessNoSandbox"],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"],
+      },
+    },
     restartOnFileChange: true,
   });
 };
